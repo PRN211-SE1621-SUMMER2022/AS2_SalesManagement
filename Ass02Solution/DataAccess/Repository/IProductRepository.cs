67a4d6df12683f1ef9565529ea6dc61466f59223
Hungpd170501 <hungpd170501@gmail.com>
2022-07-03 02:15:58 +0700

    IProductRepository

---
+++ Ass02Solution/DataAccess/Repository/IProductRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IProductRepository
+    {
+        IEnumerable<Product> GetAllProduct();
+        IEnumerable<Product> GetFilteredProduct(String tag);
+        Product GetProductById(int productId);
+        void InsertProduct(Product product);
+        void UpdateProduct(Product product);
+        void RemoveProduct(Product product);
+    }
+}
