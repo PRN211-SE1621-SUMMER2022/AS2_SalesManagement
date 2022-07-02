8a1928ee370850701f2af34848b4f2e07888871b
Hungpd170501 <hungpd170501@gmail.com>
2022-07-03 02:18:04 +0700

    ProductRepository

---
+++ Ass02Solution/DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,27 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        public IEnumerable<Product> GetAllProduct() => ProductDAO.Instance.GetAllProduct();
+
+        public IEnumerable<Product> GetFilteredProduct(string tag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetProductById(int productId) => ProductDAO.Instance.GetProductById(productId);
+
+        public void InsertProduct(Product product) => ProductDAO.Instance.InsertProduct(product);
+
+        public void RemoveProduct(Product product) => ProductDAO.Instance.RemoveProduct(product);
+
+        public void UpdateProduct(Product product) => ProductDAO.Instance.UpdateProduct(product);
+    }
+}
