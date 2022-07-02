620c0c9e7468376040b3565dc273f8f53a2a2a6a
Hungpd170501 <hungpd170501@gmail.com>
2022-07-03 02:17:28 +0700

    OrderRepository

---
+++ Ass02Solution/DataAccess/Repository/OrderRepository.cs
@@ -0,0 +1,22 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class OrderRepository : IOrderRepository
+    {
+        public void DeleteOrder(Order order) => OrderDAO.Instance.DeleteOrder(order);
+
+        public IEnumerable<Order> GetAllOrder() => OrderDAO.Instance.GetAllOrder();
+
+        public Order GetOrderByID(int orderID) => OrderDAO.Instance.GetOrderByID(orderID);
+
+        public void InsertOrder(Order order) => OrderDAO.Instance.InsertOrder(order);
+
+        public void UpdateOrder(Order order) => OrderDAO.Instance.UpdateOrder(order);
+    }
+}
