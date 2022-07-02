d62ee215ed4776695cb7784656206ed60bea30da
Hungpd170501 <hungpd170501@gmail.com>
2022-07-03 02:15:23 +0700

    IOrderRepository

---
+++ Ass02Solution/DataAccess/Repository/IOrderRepository.cs
@@ -0,0 +1,19 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> GetAllOrder();
+        Order GetOrderByID(int orderID);
+        void InsertOrder(Order order);
+        void DeleteOrder(Order order);
+        void UpdateOrder(Order order);
+
+    }
+}
