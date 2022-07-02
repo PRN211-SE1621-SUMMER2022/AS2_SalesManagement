e0b9ddb925b259c0f894883b3dc5e481fa937a27
Hungpd170501 <hungpd170501@gmail.com>
2022-07-03 02:16:58 +0700

    OrderDetailRepository

---
+++ Ass02Solution/DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,25 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repository
+{
+    public class OrderDetailRepository : IOrderDetailRepository
+    {
+        public void DeleteOrderDetail(OrderDetail orderDetail) => OrderDetailDAO.Instance.DeleteOrderDetail(orderDetail);
+
+        public IEnumerable<OrderDetail> GetAllOrderDetail() => OrderDetailDAO.Instance.GetAllOrderDetail();
+
+        public OrderDetail GetOrderDetailByID(int orderID, int productID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertOrderDetail(OrderDetail orderDetail) => OrderDetailDAO.Instance.InsertOrderDetail(orderDetail);
+
+        public void UpdateOrderDetail(OrderDetail orderDetail) => OrderDetailDAO.Instance.UpdateOrderDetail(orderDetail);
+    }
+}
